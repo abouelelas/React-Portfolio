9fcb6649e965dd614a8ca7145c72f33db6afbad3
Sarah Abouelela <abouelelas@gmail.com>
2020-10-29 14:57:45 -0400

    Builds assets

---
+++ docs/precache-manifest.235e991707d8b139038f4ec5039ca9f2.js
@@ -0,0 +1,170 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "312c4dd2e6e7e6ddc20976413b79050c",
+    "url": "/react-portfolio/index.html"
+  },
+  {
+    "revision": "7c6a64cba482341eb92e",
+    "url": "/react-portfolio/static/css/2.2a86faee.chunk.css"
+  },
+  {
+    "revision": "0706ff6b5d127698c122",
+    "url": "/react-portfolio/static/css/main.2e852682.chunk.css"
+  },
+  {
+    "revision": "7c6a64cba482341eb92e",
+    "url": "/react-portfolio/static/js/2.0bfd76af.chunk.js"
+  },
+  {
+    "revision": "44ba416b9a7217ca99c025d9f4dd1124",
+    "url": "/react-portfolio/static/js/2.0bfd76af.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0706ff6b5d127698c122",
+    "url": "/react-portfolio/static/js/main.e1d52015.chunk.js"
+  },
+  {
+    "revision": "aff33cef9dbb8787d6b3",
+    "url": "/react-portfolio/static/js/runtime-main.213b1880.js"
+  },
+  {
+    "revision": "f8648487f068e002a0ad46321d555add",
+    "url": "/react-portfolio/static/media/01-full.f8648487.jpg"
+  },
+  {
+    "revision": "eda4598198a00832fb334d5a4e588ead",
+    "url": "/react-portfolio/static/media/01-thumbnail.eda45981.jpg"
+  },
+  {
+    "revision": "8fdb36b51df820ac2e7bcf6533327f6f",
+    "url": "/react-portfolio/static/media/02-full.8fdb36b5.jpg"
+  },
+  {
+    "revision": "e52270b3af9b353d02078b01acd931ae",
+    "url": "/react-portfolio/static/media/02-thumbnail.e52270b3.jpg"
+  },
+  {
+    "revision": "3b263f0ffc3870f7fc6cefee9a86d50b",
+    "url": "/react-portfolio/static/media/03-full.3b263f0f.jpg"
+  },
+  {
+    "revision": "09c41872ba6c96c6efdcab7f202a71a7",
+    "url": "/react-portfolio/static/media/03-thumbnail.09c41872.jpg"
+  },
+  {
+    "revision": "c58223e188b4fad806838b3355441f74",
+    "url": "/react-portfolio/static/media/04-full.c58223e1.jpg"
+  },
+  {
+    "revision": "80c8ff5c4a1a0775616a5541086b1d51",
+    "url": "/react-portfolio/static/media/04-thumbnail.80c8ff5c.jpg"
+  },
+  {
+    "revision": "408a2be2fc16ddf874a8daac0f81c828",
+    "url": "/react-portfolio/static/media/05-full.408a2be2.jpg"
+  },
+  {
+    "revision": "68d8d9495f59fa5109f3b337d784cacc",
+    "url": "/react-portfolio/static/media/05-thumbnail.68d8d949.jpg"
+  },
+  {
+    "revision": "6379a8ad27dee3e9166a9f81b32db785",
+    "url": "/react-portfolio/static/media/06-full.6379a8ad.jpg"
+  },
+  {
+    "revision": "20456fff3658b40a59a73ae9821a273f",
+    "url": "/react-portfolio/static/media/06-thumbnail.20456fff.jpg"
+  },
+  {
+    "revision": "e0b34f6fc62c82231d2016e7e85d7ed0",
+    "url": "/react-portfolio/static/media/1.e0b34f6f.jpg"
+  },
+  {
+    "revision": "a90b0f86f471b7840c8c3b678620cbf3",
+    "url": "/react-portfolio/static/media/2.a90b0f86.jpg"
+  },
+  {
+    "revision": "cc41e47b8fbdb1c81c375b65071e478c",
+    "url": "/react-portfolio/static/media/2.cc41e47b.jpg"
+  },
+  {
+    "revision": "2df43e3546ec0d6666badb2ddbc716ee",
+    "url": "/react-portfolio/static/media/3.2df43e35.jpg"
+  },
+  {
+    "revision": "e6c94bc823516547b7694f975b524dad",
+    "url": "/react-portfolio/static/media/EatdaburgerSS.e6c94bc8.png"
+  },
+  {
+    "revision": "cd7c6945d71036607f761238d1df8466",
+    "url": "/react-portfolio/static/media/Proj2SS.cd7c6945.png"
+  },
+  {
+    "revision": "42936998ac04c9c0d34b43ae717d5a28",
+    "url": "/react-portfolio/static/media/assignment-screenshot2.42936998.png"
+  },
+  {
+    "revision": "7182d5797761022f2c5fd96d62e3c9f7",
+    "url": "/react-portfolio/static/media/assignment-screenshot3.7182d579.png"
+  },
+  {
+    "revision": "57323c47cf0fb60e3971796af0aa1373",
+    "url": "/react-portfolio/static/media/background1.57323c47.jpg"
+  },
+  {
+    "revision": "08a0c79f3dc15eb662c9782e52578de7",
+    "url": "/react-portfolio/static/media/background2.08a0c79f.jpg"
+  },
+  {
+    "revision": "0c6d65fa3a2653a16ed81ed30bcc2371",
+    "url": "/react-portfolio/static/media/background3.0c6d65fa.jpg"
+  },
+  {
+    "revision": "9c7f29bf198aa47aa0bf562af2472e7e",
+    "url": "/react-portfolio/static/media/background5.9c7f29bf.jpg"
+  },
+  {
+    "revision": "eba2f30b2d9c1634ddefcf30460107b8",
+    "url": "/react-portfolio/static/media/close-icon.eba2f30b.svg"
+  },
+  {
+    "revision": "556f31acd686989b1afcf382c05846aa",
+    "url": "/react-portfolio/static/media/favicon.556f31ac.ico"
+  },
+  {
+    "revision": "e2d2a51c2932205b52f4a5ae1943eb97",
+    "url": "/react-portfolio/static/media/header-bg.e2d2a51c.jpg"
+  },
+  {
+    "revision": "f40ed5b1e791e9d2479ba8b5e67f2df8",
+    "url": "/react-portfolio/static/media/map-image.f40ed5b1.png"
+  },
+  {
+    "revision": "2c06174927f9ff7f6e693f3106bd5ff7",
+    "url": "/react-portfolio/static/media/navbar-logo.2c061749.svg"
+  },
+  {
+    "revision": "beff3807bf881459d222e0c5f9991244",
+    "url": "/react-portfolio/static/media/portfolioss1.beff3807.png"
+  },
+  {
+    "revision": "fb6d572f5a1b0c1d250f1c30e0bfcb5b",
+    "url": "/react-portfolio/static/media/portfolioss2.fb6d572f.png"
+  },
+  {
+    "revision": "3ac7a0cc43fdafe148d9f893b08a6c96",
+    "url": "/react-portfolio/static/media/portfolioss3.3ac7a0cc.png"
+  },
+  {
+    "revision": "27ecc119d880b2858c6eab90d7dc29ba",
+    "url": "/react-portfolio/static/media/resume2020.27ecc119.pdf"
+  },
+  {
+    "revision": "3ed21a8ec57d7a7ffb1da3f2252b68ae",
+    "url": "/react-portfolio/static/media/simpli-stocks-ss.3ed21a8e.png"
+  },
+  {
+    "revision": "c4577c306398574d8362da3040177c8f",
+    "url": "/react-portfolio/static/media/weather-dashboard-ss.c4577c30.png"
+  }
+]);
--- docs/service-worker.js
+++ docs/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/React-Portfolio/precache-manifest.3583dfebf165d9cb52245336d484845a.js"
+  "/react-portfolio/precache-manifest.235e991707d8b139038f4ec5039ca9f2.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/React-Portfolio/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/react-portfolio/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
